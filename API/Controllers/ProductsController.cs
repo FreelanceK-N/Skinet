8836b81f43dc12024dc3a2ab4c260d61630fb9bb
moka <konstantin.marinov17@zuhlke.com>
2023-03-15 11:10:03 +0200

    Added Dto layer

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using API.Dto;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
@@ -26,22 +28,41 @@ namespace API.Controllers
             this._productTypesRepo = productTypesRepo;
         }
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts() 
+        public async Task<ActionResult<List<ProductDto>>> GetProducts() 
         {
             var spec = new ProductsWithTypesAndBrandsSpecification();
 
             var products = await this._productsRepo.ListAsync(spec);
 
-            return Ok(products);
+            return products.Select(product => new ProductDto 
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand.Name,
+                ProductType = product.ProductType.Name 
+            }).ToList();
         }
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await this._productsRepo.GetEntityWithSpecAsync(spec);
 
-            return Ok(product);
+            return new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand.Name,
+                ProductType = product.ProductType.Name
+            };
+
         }
 
         [HttpGet("brands")]
