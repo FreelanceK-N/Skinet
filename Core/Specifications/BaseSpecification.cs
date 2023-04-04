ecb4d393c7723818fbfc9a2475eef413a187892b
moka <konstantin.marinov17@zuhlke.com>
2023-04-04 11:10:48 +0300

    Added sorting specification.

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -11,7 +11,11 @@ namespace Core.Specifications
 
         public List<Expression<Func<T, object>>> Includes {get; } = new List<Expression<Func<T, object>>>();
 
-         public BaseSpecification(Expression<Func<T, bool>> criteria)
+        public Expression<Func<T, object>> OrderBy {get; private set;}
+
+        public Expression<Func<T, object>> OrderByDescending {get; private set;}
+
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
         {
             Criteria = criteria;
         }
@@ -22,6 +26,16 @@ namespace Core.Specifications
 
         protected void AddInclude(Expression<Func<T, object>> includeExpression) {
             Includes.Add(includeExpression);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
         }
     } 
 }
