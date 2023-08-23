b658613cd613e3cb89d4837a2d397aa7243096d0
moka <konstantin.marinov17@zuhlke.com>
2023-08-23 17:58:37 +0300

    Added nav bar component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient
+      .get<Pagination<Product[]>>(
+        'https://localhost:5001/api/products?pageSize=50'
+      )
+      .subscribe({
+        next: (products) => (this.products = products.data),
+        error: (error) => console.log(error),
+      });
+  }
 }
