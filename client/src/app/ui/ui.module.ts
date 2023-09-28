9a7943fa21d1079006e7d1e58c6cf521293b243e
moka <konstantin.marinov17@zuhlke.com>
2023-09-28 18:33:55 +0300

    Changed folder structure

--- client/src/app/ui/ui.module.ts
+++ client/src/app/ui/ui.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShopComponent } from './components/pages/shop/shop.component';
+import { ShopComponent } from './page-components/shop/shop.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductFiltersComponent } from './components/filters/product-filters/product-filters.component';
 import { ProductsGridComponent } from './components/grids/products-grid/products-grid.component';
