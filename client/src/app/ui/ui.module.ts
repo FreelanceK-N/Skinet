6dd57d524821b9511aca32e51a71e0f3a17f54f6
moka <konstantin.marinov17@zuhlke.com>
2023-09-28 18:24:28 +0300

    Added filter and products grid files

--- client/src/app/ui/ui.module.ts
+++ client/src/app/ui/ui.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './components/pages/shop/shop.component';
 import { ProductComponent } from './components/product/product.component';
+import { ProductFiltersComponent } from './components/filters/product-filters/product-filters.component';
+import { ProductsGridComponent } from './components/grids/products-grid/products-grid.component';
 
 @NgModule({
-  declarations: [ShopComponent, ProductComponent],
+  declarations: [ShopComponent, ProductComponent, ProductFiltersComponent, ProductsGridComponent],
   imports: [CommonModule],
   exports: [ShopComponent],
 })
