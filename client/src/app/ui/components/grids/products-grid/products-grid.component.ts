ed8e3d9f1580aafee5d79cf473598c995d873575
moka <konstantin.marinov17@zuhlke.com>
2023-09-29 16:29:41 +0300

    Added global styles and basic elements

--- client/src/app/ui/components/grids/products-grid/products-grid.component.ts
+++ client/src/app/ui/components/grids/products-grid/products-grid.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-products-grid',
   templateUrl: './products-grid.component.html',
-  styleUrls: ['./products-grid.component.scss']
+  styleUrls: ['./products-grid.component.scss'],
 })
 export class ProductsGridComponent {
-
+  @Input() products: Product[] = [];
 }
